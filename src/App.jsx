4fcbea50fd3f67eb8b3a592425e24a588cfb945f
BlackRing8 <gprakoso123@gmail.com>
2023-12-18 18:32:59 +0700

    update navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Popular from "./pages/Popular";
-import MyList from "./pages/MyList";
+import Mylist from "./pages/My-list";
 import { NavBar } from "./components/Navbar";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/popular" element={<Popular/>}/>
-        <Route path="/mylist" element={<MyList/>}/>
+        <Route path="/mylist" element={<Mylist/>}/>
       </Routes>
      </div>
     </div>
