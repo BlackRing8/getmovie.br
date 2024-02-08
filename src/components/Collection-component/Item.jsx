69ffdb869d42927df6d6f78f42ec688dca024098
BlackRing8 <gprakoso123@gmail.com>
2024-02-08 15:03:20 +0700

    page collection awal

---
+++ src/components/Collection-component/Item.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Item = () => {
+  return (
+    <div>
+        <div className="flex bgCustom2 w-44 h-60 rounded-[16px]">
+        <div className="absolute flex w-20 h-8 bg-cr-2 ml-3 mt-3 rounded-lg items-center justify-center">
+          <label className="font-semibold">ON SALE</label>
+        </div>
+        <div className="flex w-44 h-60 rounded-[16px] items-end">
+          <div className="flex grid-cols-1 bg-cr-3 w-44 h-16 rounded-b-[16px] justify-center text-center">
+            <a className="mt-1" href="" id="test">
+              <p className="font-light text-white">NAMA PRODUK</p>
+              <span className="text-lg font-bold text-white">Rp.75.000,00</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Item
