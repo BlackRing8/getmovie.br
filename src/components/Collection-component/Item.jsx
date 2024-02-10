91ebcf289aa324c8e5dcdbc8dfbbf5ad85491794
BlackRing8 <gprakoso123@gmail.com>
2024-02-10 14:45:05 +0700

    layout

--- src/components/Collection-component/Item.jsx
+++ src/components/Collection-component/Item.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Item = () => {
   return (
-    <div>
-        <div className="flex bgCustom2 w-44 h-60 rounded-[16px]">
+    <div className="grid grid-cols-2 gap-2 sm:flex sm:flex-wrap w-auto h-screen pt-[60px] ml-2 mr-2">
+      <div className="flex bgCustom2 w-44 h-60 rounded-[16px]">
+        <div className="absolute flex w-20 h-8 bg-cr-2 ml-3 mt-3 rounded-lg items-center justify-center">
+          <label className="font-semibold">ON SALE</label>
+        </div>
+        <div className="flex w-44 h-60 rounded-[16px] items-end">
+          <div className="flex grid-cols-1 bg-cr-3 w-44 h-16 rounded-b-[16px] justify-center text-center">
+            <a className="mt-1" href="" id="test">
+              <p className="font-light text-white">NAMA PRODUK</p>
+              <span className="text-lg font-bold text-white">Rp.75.000,00</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex bgCustom2 w-44 h-60 rounded-[16px]">
+        <div className="absolute flex w-20 h-8 bg-cr-2 ml-3 mt-3 rounded-lg items-center justify-center">
+          <label className="font-semibold">ON SALE</label>
+        </div>
+        <div className="flex w-44 h-60 rounded-[16px] items-end">
+          <div className="flex grid-cols-1 bg-cr-3 w-44 h-16 rounded-b-[16px] justify-center text-center">
+            <a className="mt-1" href="" id="test">
+              <p className="font-light text-white">NAMA PRODUK</p>
+              <span className="text-lg font-bold text-white">Rp.75.000,00</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex bgCustom2 w-44 h-60 rounded-[16px]">
+        <div className="absolute flex w-20 h-8 bg-cr-2 ml-3 mt-3 rounded-lg items-center justify-center">
+          <label className="font-semibold">ON SALE</label>
+        </div>
+        <div className="flex w-44 h-60 rounded-[16px] items-end">
+          <div className="flex grid-cols-1 bg-cr-3 w-44 h-16 rounded-b-[16px] justify-center text-center">
+            <a className="mt-1" href="" id="test">
+              <p className="font-light text-white">NAMA PRODUK</p>
+              <span className="text-lg font-bold text-white">Rp.75.000,00</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex bgCustom2 w-44 h-60 rounded-[16px]">
         <div className="absolute flex w-20 h-8 bg-cr-2 ml-3 mt-3 rounded-lg items-center justify-center">
           <label className="font-semibold">ON SALE</label>
         </div>
@@ -17,7 +56,7 @@ const Item = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
--- src/pages_/Collection.jsx
+++ src/pages_/Collection.jsx
@@ -3,7 +3,7 @@ import Item from "../components/Collection-component/Item";
 
 const Collection = () => {
   return (
-    <div className="grid grid-cols-2 gap-2 sm:flex sm:flex-wrap w-auto h-screen pt-[60px] ml-2 mr-2">
+    <div >
       <Item />
     </div>
   );
